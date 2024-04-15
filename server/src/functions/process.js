d74d8bff5a5c4b727049a2c8b81ae174f2eeadfa
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2024-04-15 15:45:33 +0300

    fix

--- server/src/functions/process.js
+++ server/src/functions/process.js
@@ -1,6 +1,6 @@
 const { app } = require("@azure/functions");
 const { extraction_balance_pdf } = require("../process/extraction_balance_pdf");
-app.http("httpTrigger", {
+app.http("process", {
   methods: ["GET", "POST"],
   authLevel: "anonymous",
   handler: async (request, context) => {
