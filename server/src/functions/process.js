35d9e884fb99101d2f0eda19eeb9b1a748a78ca2
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2024-04-15 18:51:04 +0300

    pdf

--- server/src/functions/process.js
+++ server/src/functions/process.js
@@ -1,10 +1,26 @@
 const { app } = require("@azure/functions");
 const { extraction_balance_pdf } = require("../process/extraction_balance_pdf");
+const url = require("url");
+
 app.http("process", {
   methods: ["GET", "POST"],
   authLevel: "anonymous",
   handler: async (request, context) => {
-    const object = await extraction_balance_pdf();
-    return object;
+    const pdfPath = request.query.get("pdfPath");
+
+    if (!pdfPath) {
+      context.res = {
+        status: 400,
+        body: "PDF path is required",
+      };
+      return;
+    }
+
+    const object = await extraction_balance_pdf(pdfPath);
+    console.log("Object:", object);
+    context.res = {
+      status: 200,
+      body: object,
+    };
   },
 });
